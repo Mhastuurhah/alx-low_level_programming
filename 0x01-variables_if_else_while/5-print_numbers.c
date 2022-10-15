10aa63521a55cd0831759416bf1d19053f49f4c0
Mhastuurhah <abidexmosh@yahoo.com>
2022-10-15 05:58:21 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print single number digits
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; ++num)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+	return (0);
+}
