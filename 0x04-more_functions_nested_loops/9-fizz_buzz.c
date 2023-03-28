56fa1130afab46a8cb9d8367d4ee89979ca7ee5b
Mhastuurhah <70760998+Mhastuurhah@users.noreply.github.com>
2023-03-28 19:03:13 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i < 100; i++)
 	{
 		if (i % 15 == 0)
 			printf("FizzBuzz ");
@@ -20,6 +20,7 @@ int main(void)
 		else
 			printf("%d ", i);
 	}
+	printf("Buzz");
 	printf("\n");
 
 	return (0);
