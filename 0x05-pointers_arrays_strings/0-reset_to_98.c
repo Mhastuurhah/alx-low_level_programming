dc36a0cc18eb5297d9b95e96d1f859163923a83d
Mhastuurhah <mhastuurhah@gmail.com>
2023-03-29 13:27:20 +0100

    created a new folder, added readme, and two starter files

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int
+ * Description: takes a pointer to an int
+ * @n: integer pointer
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
